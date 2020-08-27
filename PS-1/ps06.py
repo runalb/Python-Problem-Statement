b92591bd2f5d77138e3bde7037065939cb229c47
Runal <65166862+runalb@users.noreply.github.com>
2020-08-27 23:54:12 +0530

    Create ps06.py

---
+++ PS-1/ps06.py
@@ -0,0 +1,37 @@
+# 06 WAP to accept 3 numbers and display smallest and largest number
+
+num_l = []
+for x in range(3):
+    no = float(input("Enter no. :"))
+    num_l.append(no)
+
+print(num_l)
+
+#using min max function
+print("= Using min, max function =")
+print("Smallest",min(num_l))
+print("largest",max(num_l))
+
+
+
+#2nd soln-
+print("\n\n====== 2nd soln ======")
+
+largest = None
+smallest = None
+for x in range(3):
+    num = float(input("Enter a number: "))
+    try:
+        if largest is None:
+            largest = num
+        if smallest is None:
+            smallest = num
+        if num > largest:
+            largest = num
+        if num < smallest:
+            smallest = num
+    except:
+        print ("Invalid input")
+
+print ("Minimum is", smallest)
+print ("largest is", largest)
