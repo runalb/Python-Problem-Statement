25c2e79367a03469bf0938522979b0ffa6c527d7
Runal <65166862+runalb@users.noreply.github.com>
2020-08-27 23:53:09 +0530

    Create ps04.py

---
+++ PS-1/ps04.py
@@ -0,0 +1,12 @@
+# 04 WAP to accept radius of circle and calculate area and circumference of circle
+
+r = float(input("Enter radius of circle: "))
+
+#Aera of circle
+area =  3.14 * r * r
+
+#Circumference of circle
+circum = 2 * 3.14 * r
+
+print("Area:",area)
+print("Circumference:",circum)
