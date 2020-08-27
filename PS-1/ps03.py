c2649aad065ccd0150dde552dc918d06e62866e9
Runal <65166862+runalb@users.noreply.github.com>
2020-08-27 23:52:44 +0530

    Create ps03.py

---
+++ PS-1/ps03.py
@@ -0,0 +1,9 @@
+# 03 WAP to accept side of square and calculate area and perimeter of square
+
+side = float(input("Enter length of side: "))
+
+area = side * side
+peri = 4 * side
+
+print("Area of Square:",area)
+print("Perimeter of Square:",peri)
