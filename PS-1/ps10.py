7b82aab7d113121337c18265955b4599aa85136c
Runal <65166862+runalb@users.noreply.github.com>
2020-08-29 17:34:06 +0530

    Create ps10.py
    WAP to display the examination schedule (extract the date from exam_st_date)
    exam_st_date = (11,12,2018)
    Sample Output: The examination will start from : 11/12/2012

---
+++ PS-1/ps10.py
@@ -0,0 +1,7 @@
+# 10 WAP to display the examination schedule (extract the date from exam_st_date)
+# exam_st_date = (11,12,2018)
+# Sample Output: The examination will start from : 11/12/2012
+
+exam_st_date = (11,12,2018)
+
+print("The examination will start from : {}/{}/{}".format(exam_st_date[0],exam_st_date[1],exam_st_date[2]))
