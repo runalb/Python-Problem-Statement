b9fd662412e77055bfc8c06ed68a9670a106a38b
Runal <65166862+runalb@users.noreply.github.com>
2020-09-21 23:13:38 +0530

    Extract the date from tuple and print wrt
    WAP to display the examination schedule (extract the date from exam_st_date)
    exam_st_date = (11,12,2018)
    Sample Output: The examination will start from : 11/12/2012

--- PS-1/ps10.py
+++ PS-1/ps10.py
@@ -1,4 +1,4 @@
-# 10 WAP to display the examination schedule (extract the date from exam_st_date)
+# PS-10 WAP to display the examination schedule (extract the date from exam_st_date)
 # exam_st_date = (11,12,2018)
 # Sample Output: The examination will start from : 11/12/2012
 
