84f1574ad9a086b9a2de7078175544cfcef04588
Runal <65166862+runalb@users.noreply.github.com>
2020-09-21 22:56:47 +0530

    WAP to accept two numbers and display their sum

--- PS-1/ps02.py
+++ PS-1/ps02.py
@@ -1,4 +1,4 @@
-# 02 WAP to accept two numbers and display their sum
+# PS-02 WAP to accept two numbers and display their sum
 
 a = float(input("Enter 1st no: "))
 b = float(input("Enter 1st no: "))
