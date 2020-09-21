3002d21c3a78daa9de9fdf5d0971cd13bd70f353
Runal <65166862+runalb@users.noreply.github.com>
2020-09-21 23:23:21 +0530

    Accept 3 no. & display smallest and largest number
    WAP to accept 3 numbers and display the smallest and largest number

--- PS-1/ps06.py
+++ PS-1/ps06.py
@@ -35,3 +35,22 @@ for x in range(3):
 
 print ("Minimum is", smallest)
 print ("largest is", largest)
+
+
+
+
+
+#3rd soln-
+print("\n\n====== 3rd soln ======")
+
+no=input("Enter no: ")
+for x in range(9,-1,-1):
+    temp=str(x)
+    if(temp in no):
+        print("Large: ",temp)
+        break
+for x in range(10):
+    temp=str(x)
+    if(temp in no):
+        print("small: ",temp)
+        break
