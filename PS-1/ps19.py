a39cda09ced7e5e77e105d084ea80893ccd34f6c
Runal <65166862+runalb@users.noreply.github.com>
2021-03-01 12:26:02 +0530

    WAP to display following pattern
    WAP to display following pattern
    1
    22
    333
    4444
    55555
    666666
    7777777
    88888888
    999999999

---
+++ PS-1/ps19.py
@@ -0,0 +1,17 @@
+# PS-19 WAP to display following pattern
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+# 88888888
+# 999999999
+
+for rows in range(1,9):
+    for j in range(rows):
+        print(rows, end="")
+    print()
+
+
