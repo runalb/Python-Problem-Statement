8045d3bad41ddd2c6b28a6b807162130c72faa36
Runal <65166862+runalb@users.noreply.github.com>
2021-03-01 12:22:04 +0530

    WAP to accept name and number. Display name for given numbers of times

---
+++ PS-1/ps17.py
@@ -0,0 +1,7 @@
+# PS-17 WAP to accept name and number. Display name for given numbers of times
+
+name = input("Enter name: ")
+number = int(input("Enter no: "))
+
+for x in range(number):
+    print(name)
