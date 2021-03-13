eb9edbb6147ff60615a463ecaf7a5c230a1833dd
Runal <65166862+runalb@users.noreply.github.com>
2021-03-13 12:12:02 +0530

    WAP to print the following string in a specific format (see the output).
    Sample String = "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are!"
    
    Output:
    Twinkle, twinkle, little star,
            How I wonder what you are!
    Up above the world so high,
            Like a diamond in the sky.
    Twinkle, twinkle, little star,
            How I wonder what you are!"

---
+++ PS-1/ps28.py
@@ -0,0 +1,14 @@
+# PS-28 WAP to print the following string in a specific format (see the output).
+'''
+Sample String = "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are!"
+
+Output:
+Twinkle, twinkle, little star,
+        How I wonder what you are!
+Up above the world so high,
+        Like a diamond in the sky.
+Twinkle, twinkle, little star,
+        How I wonder what you are!"
+'''
+
+print("Twinkle, twinkle, little star,\n\t\tHow I wonder what you are!\nUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are!")
