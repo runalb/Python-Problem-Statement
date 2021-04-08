879da8d085200a098bfbb1d67076be2c7ba61f8c
Runal <runalb@users.noreply.github.com>
2021-04-08 19:38:09 +0530

    WAP to accepts a number between 1 to 12 and display corresponding month .

---
+++ PS-1/ps41.py
@@ -0,0 +1,6 @@
+# PS-41 WAP to accepts a number between 1 to 12 and display corresponding month .
+
+li_month =['January','February','March','April','May','June','July','August','September','October','November','December']
+
+no = int(input("Enter no: "))
+print(li_month[no-1])
