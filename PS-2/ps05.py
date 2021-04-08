63cf8c4ca4ae689dfc73ae5a3a7e83f20a18e3b4
Runal <runalb@users.noreply.github.com>
2021-04-08 19:40:49 +0530

    WAP to check whether a number is positive or negative

---
+++ PS-2/ps05.py
@@ -0,0 +1,8 @@
+# PS-05 WAP to check whether a number is positive or negative
+
+no = int(input("Enter no: "))
+
+if no >= 0:
+    print(no,"is Positive no.")
+else:
+    print(no,"is Negative no.")
