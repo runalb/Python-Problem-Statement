203e42be7579e3a8c94c187cee2b6458b3b2b0e3
Runal <runalb@users.noreply.github.com>
2021-04-11 17:24:35 +0530

    WAP to put Even and Odd element in a list into two different lists

---
+++ PS-2/ps20.py
@@ -0,0 +1,29 @@
+# PS-20 WAP to put Even and Odd element in a list into two different lists
+
+def check_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+even_li = []
+odd_li = []
+
+user_input = ""
+while user_input.upper() != "Y":
+    print("To stop enter 'Y'")
+    user_input = input("Enter no: ")
+    try:
+        if check_even(int(user_input)):
+            even_li.append(int(user_input))
+            print("{} Added to Even List\n".format(user_input))
+        else:
+            odd_li.append(int(user_input))
+            print("{} Added to Odd List\n".format(user_input))
+
+    except:
+        print("Enter Integer no. OR To stop enter 'Y'\n")
+
+
+print("Even list\n",even_li)
+print("Odd list\n",odd_li)
