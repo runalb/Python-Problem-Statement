262d8e2a79b73865c129a33fd3a1d799b2debf7d
Runal <runalb@users.noreply.github.com>
2021-04-23 13:32:05 +0530

    WAP to form new string where the first character and the last character have been exchanged

--- PS-2/ps38.py
+++ PS-2/ps38.py
@@ -1,21 +1,11 @@
 # PS-38 WAP to form new string where the first character and the last character have been exchanged
 
 def exchange_1st_n_last_ch(str):
-    str_li = []
-    for ch in str:
-        str_li.append(ch)
+    start = string[0]
+    end = string[-1]
 
-    # exchanging 1st ch to last ch
-    first_ele = str_li[0]
-    last_ele = str_li[-1]
-
-    str_li[0] = last_ele
-    str_li[-1] = first_ele
-
-    res = ''
-    for ch in str_li:
-        res = res + ch
-    return res
+    exchange_string = end + string[1:-1] + start
+    return exchange_string
 
 
 string = "Hello all"
