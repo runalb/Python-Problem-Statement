a96150ace5a85f348a39a148a4ddf77a66a4907f
Runal <runalb@users.noreply.github.com>
2021-04-23 13:15:12 +0530

    WAP to remove the nth index character from a non-empty string

---
+++ PS-2/ps36.py
@@ -0,0 +1,27 @@
+# PS-36 WAP to remove the nth index character from a non-empty string
+
+def remove_nth_idx_character(str,idx):
+    c = 0
+    res = ''
+
+    for ch in string:
+        c = c + 1
+        if c == idx:
+            pass
+        else:
+            res = res + ch
+    return res
+
+
+
+string = "Hello all"
+print("Original String:\n",string)
+
+idx = int(input(("\nEnter index to remove character: ")))
+
+string = remove_nth_idx_character(string,idx)
+print("\nString After removing {} idx character:\n {}".format(idx,string))
+
+
+
+
